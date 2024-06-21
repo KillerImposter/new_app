09fa06c17492f55adc6b416b4d217a1f97ca9a9d
shaitkhetan <shamitkhetan04@gmail.com>
2024-06-21 19:42:05 +0530

    commit 1

--- App.tsx
+++ App.tsx
@@ -5,9 +5,12 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState, useEffect } from 'react';
+import type { PropsWithChildren } from 'react';
 import {
+  Alert,
+  Linking,
+  PermissionsAndroid,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -25,11 +28,14 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Contacts, { Contact } from 'react-native-contacts';
+import RNCallKeep from 'react-native-callkeep';
+
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
+function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -57,41 +63,90 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [contacts, setContacts] = useState<Contact[]>([]);
+
+  useEffect(() => {
+    askPermission()
+      .then(() => {
+        readContacts().then(() => {
+          setupCallKeep();
+        })
+      });
+  }, []);
+
+  async function readContacts() {
+    PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_CONTACTS).then(async () => {
+      const contactList = await Contacts.getAllWithoutPhotos();
+      setContacts(contactList);
+    });
+  }
+
+  async function askPermission() {
+    try {
+      const permissionStatus = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_CALL_LOG,
+        PermissionsAndroid.PERMISSIONS.PROCESS_OUTGOING_CALLS,
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+        PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE
+      ]);
+      console.log('READ_CALL_LOG: ' + permissionStatus['android.permission.READ_CALL_LOG']);
+      console.log('READ_CONTACTS: ' + permissionStatus['android.permission.READ_CONTACTS']);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function setupCallKeep() {
+    try {
+      RNCallKeep.setup({
+        ios: {
+          appName: 'CallKeepDemo',
+        },
+        android: {
+          alertTitle: 'Permissions required',
+          alertDescription: 'This application needs to access your phone accounts',
+          cancelButton: 'Cancel',
+          additionalPermissions: [PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE],
+          okButton: 'ok',
+        },
+      })
+        .then(async() => {
+          RNCallKeep.setAvailable(true);  // You can now start receiving calls
+          console.log('RNCallkeep setup successfully');
+          const permissionStatus = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE)
+          if(permissionStatus)
+             console.log('READ_PHONE_STATE: granted');
+          else
+            console.log('READ_PHONE_STATE: not granted');
+        })
+        .catch((err) => console.log(`RNCallKeep setup error: ${err}`));
+    }
+    catch (err) {
+      console.log('initializeCallKeep error:', err)
+    }
+  }
+
+  // Code to log the contacts
+  // useEffect(() => {
+  //   contacts.forEach(contact => {
+  //     console.log(`${contact.displayName}: `);
+  //     contact.phoneNumbers.forEach((Details) => { console.log(Details.number) });
+  //   });
+  // }, [contacts]);
+
+  useEffect(() => {
+    RNCallKeep.addEventListener('didReceiveStartCallAction', () => console.log('Incoming call'));
+    RNCallKeep.addEventListener('answerCall', () => console.log('Answered call'));
+  }, []);
+
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Section title='Hi'></Section>
     </SafeAreaView>
   );
 }
