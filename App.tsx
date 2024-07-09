d121f516a17b0d6308f8c8d12eb44232919ccf41
shaitkhetan <shamitkhetan04@gmail.com>
2024-07-09 15:27:16 +0530

    Added background services and cleaned up redundant code

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import CallDetectorManager from 'react-native-call-detection';
 import Contacts, { Contact } from 'react-native-contacts';
+import BackgroundService from 'react-native-background-actions';
 
 const App = () => {
   const [featureOn, setFeatureOn] = useState(false);
@@ -15,6 +16,7 @@ const App = () => {
   const [belongsToContacts, setBelongsToContacts] = useState(false);
   const [number, setNumber] = useState(null);
   const [contacts, setContacts] = useState<Contact[]>([]);
+
   useEffect(() => {
     const setupPermissions = async () => {
       await askPermission();
@@ -22,17 +24,14 @@ const App = () => {
     };
     setupPermissions();
   }, []);
-  
+
   useEffect(() => {
     contacts.forEach(contact => {
-      console.log(`${contact.displayName}: `);
-      contact.phoneNumbers.forEach((numberDetails) => { 
-        numberDetails.number = numberDetails.number.replaceAll("-","");
-        numberDetails.number = numberDetails.number.replaceAll(" ","");
-        numberDetails.number = numberDetails.number.replaceAll("(","");
-        numberDetails.number = numberDetails.number.replaceAll(")","");
-        console.log(numberDetails.number);
-       });
+      // console.log(`${contact.displayName}: `);
+      contact.phoneNumbers.forEach((numberDetails) => {
+        numberDetails.number = numberDetails.number.replace(/[- )(]/g, '');
+        // console.log(numberDetails.number);
+      });
     });
   }, [contacts]);
 
@@ -44,23 +43,28 @@ const App = () => {
         PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
         PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE
       ]);
-      console.log('READ_CALL_LOG: ' + permissionStatus['android.permission.READ_CALL_LOG']);
-      console.log('READ_CONTACTS: ' + permissionStatus['android.permission.READ_CONTACTS']);
-      console.log('READ_PHONE_STATE: ' + permissionStatus['android.permission.READ_PHONE_STATE']);
-    }
-    catch (err) {
+      console.log('Permissions: ', permissionStatus);
+    } catch (err) {
       console.log(err);
     }
   }
 
   async function readContacts() {
-    PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_CONTACTS).then(async () => {
+    const hasPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_CONTACTS);
+    if (hasPermission) {
       const contactList = await Contacts.getAllWithoutPhotos();
       setContacts(contactList);
-    });
+    }
   }
 
-  function startDetection() {
+  async function startBackgeoundService (taskDataArguments)
+  {
+    await new Promise((resolve) => {
+      startDetection();
+    });
+  };
+
+  async function startDetection() {
     setFeatureOn(true);
     this.callDetector = new CallDetectorManager(
       (event, number) => {
@@ -74,8 +78,7 @@ const App = () => {
           setNumber(number);
           contacts.forEach(contact => {
             contact.phoneNumbers.forEach((numberDetails) => {
-              if(numberDetails.number === number)
-              {
+              if (numberDetails.number === number) {
                 console.log(numberDetails.number, `belongs to contacts`);
                 setBelongsToContacts(true);
               }
@@ -90,19 +93,43 @@ const App = () => {
           setNumber(null);
         }
       },
-      true, // if you want to read the phone number of the incoming call [ANDROID], otherwise false
-      () => { }, // callback if your permission got denied [ANDROID] [only if you want to read incoming number] default: console.error
+      true,
+      () => { },
       {
         title: 'Phone State Permission',
-        message:
-          'This app needs access to your phone state in order to react and/or to adapt to incoming calls.',
-      }, // a custom permission request message to explain to your user, why you need the permission [recommended] - this is the default one
+        message: 'This app needs access to your phone state in order to react and/or to adapt to incoming calls.',
+      },
     );
   };
+
   function stopDetection() {
     this.callDetector && this.callDetector.dispose();
     setFeatureOn(false);
     setIncoming(false);
+  };
+
+  async function startService() {
+    const options = {
+      taskName: 'My warmcall app',
+      taskTitle: 'Warmcall app',
+      taskDesc: 'App is running in background',
+      taskIcon: {
+        name: 'ic_launcher',
+        type: 'mipmap',
+      },
+      color: '#ff00ff',
+      parameters: {
+        delay: 5000,
+      },
+    };
+
+    await BackgroundService.start(startBackgeoundService, options);
+    await BackgroundService.updateNotification({ taskDesc: 'New Example description' });
+  };
+
+  async function stopService() {
+    await BackgroundService.stop();
+    stopDetection();
   };
 
   return (
@@ -115,7 +142,7 @@ const App = () => {
       </Text>
       <TouchableHighlight
         style={{ borderRadius: 75 }}
-        onPress={featureOn ? stopDetection : startDetection}>
+        onPress={featureOn ? stopService : startService}>
         <View
           style={{
             width: 200,
